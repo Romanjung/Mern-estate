8e0404fec7244e8f13295c06d502ebb0428d9efd
Roman jung <peruhanglimbu@gmail.com>
2023-12-01 09:57:53 +0545

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-                |<Link to='/'> <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <Link to='/'> <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                     <span className='text-slate-500'>Peru</span>
                     <span className='text-slate-700'>Estate</span>
                 </h1>
